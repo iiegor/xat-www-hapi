52aa29885161246d1c05bc88f64d2b3028edfdab
iiegor <iegormundo@hotmail.com>
2015-10-07 20:29:58 +0200

    Update profile page

--- facets/user/profile.js
+++ facets/user/profile.js
@@ -1,6 +1,15 @@
+var UserModel = require('../../models/user');
+
 module.exports = function(request, reply) {
+  var loggedInUser = request.loggedInUser;
+  var profileName = request.params.username;
+
+  var User = UserModel.new(request);
+
   var opts = {
-    profileName: request.params.username
+    profileName: profileName,
+    isSelf: loggedInUser && profileName === loggedInUser.username,
+    isStaff: true
   };
 
   reply.view('user/profile', opts).code(200);
